8469c6c9ce9d1778f939ddf77d805b37fee81d4b
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-26 02:11:52 +0700

    add text mesh pro, webgl package switch to build webgl add MainMenu scene, change scene main to GamePlay, add function start game to load scene GamePlay and function to exit game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
