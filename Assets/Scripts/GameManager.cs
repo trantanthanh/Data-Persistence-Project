498ae798fbaa688095ccdb1920a61c3777794e6e
Thành Trần Tấn <trantanthanh@gmail.com>
2024-02-26 02:33:26 +0700

    add GameManager, singleton GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -8,6 +11,59 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    public string playerName = "";
+    public string bestScoreName = "";
+    public int bestScore = 0;
+
+    private string saveFileName = "savefile.json";
+    private string pathSave;
+    void Awake()
+    {
+        pathSave = $"{Application.persistentDataPath}/{saveFileName}";
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;        
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadScore();
+    }
+
+    [Serializable]
+    class SaveData
+    { 
+        public string bestScoreName = "";
+        public int bestScore;
+        public string playerName = "";
+    }
+
+    public void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.playerName = playerName;
+        data.bestScoreName = bestScoreName;
+        data.bestScore = bestScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(pathSave, json);
+    }
+
+    public void LoadScore()
+    {
+        if (File.Exists(pathSave))
+        {
+            string json = File.ReadAllText(pathSave);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            playerName = data.playerName;
+            bestScoreName = data.bestScoreName;
+            bestScore = data.bestScore;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
